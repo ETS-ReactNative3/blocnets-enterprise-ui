ea89a18cf32258a0afc2f2fb9c389deac495dbaa
Catherine Miranda <cmiranda@blocnets.com>
2018-08-30 18:11:44 -0400

    Footer + Reformat App.js
    Footer + Reformat App.js

--- src/App.js
+++ src/App.js
@@ -1,118 +1,128 @@
 import React, {Component} from 'react';
+import './App.css';
 import {
     BrowserRouter as Router,
     Route
 } from 'react-router-dom';
+import {authenticate} from './redux/actions/main.actions';
+import {connect} from 'react-redux';
 import DocumentReviewEntryView
     from './components/document-review-and-entry-view/document-dashboard/document.review.entry.view';
+import TrackAndTraceResultsView from './components/track-and-trace/views/track-and-trace.results.view';
 import ShippingView from './components/shipping-view/shipping.view';
 import ReceivingView from './components/receiving-view/receiving.view';
 import BillOfMaterials from './components/bill-of-materials/bill-of-materials';
 import DocumentSendView from './components/document-review-and-entry-view/document-send/document.send.view';
 import logo from './blocknetwhite-1.png';
-import appBarLogo from './rsz_1blocknetwhite.png';
 import paperLogo from './blocnets-logo.png'
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import AppBar from 'material-ui/AppBar';
-import Badge from '@material-ui/core/Badge';
-import Drawer from 'material-ui/Drawer';
 import Grid from '@material-ui/core/Grid';
-import IconButton from '@material-ui/core/IconButton';
+import {Toolbar, ToolbarTitle} from 'material-ui/Toolbar'
+import {FormControl} from '@material-ui/core';
+import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import InputAdornment from '@material-ui/core/InputAdornment';
-import InputLabel from '@material-ui/core/InputLabel';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import Paper from 'material-ui/Paper';
-import {Toolbar, ToolbarTitle} from 'material-ui/Toolbar'
-import './App.css';
+import Typography from '@material-ui/core/Typography';
 import SearchIcon from '@material-ui/icons/Search';
-import UserIcon from '@material-ui/icons/AccountCircleRounded';
-import {FormControl} from '@material-ui/core';
-import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import MailIcon from '@material-ui/icons/Mail';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import {authenticate} from './redux/actions/main.actions';
-import TrackAndTraceResultsView from './components/track-and-trace/views/track-and-trace.results.view';
+import UserIcon from '@material-ui/icons/AccountCircleRounded';
 
 const theme = createMuiTheme({
     palette: {
-        type: 'dark', // Switching the dark mode on is a single property value change.
+        type: 'dark'
     },
 });
 
+const appBarLogoStyle = {
+    maxWidth: 50,
+    maxHeight: 50,
+    paddingTop: 10
+};
+
 const paperStyle = {
-    height: '85%',
     width: "70%",
+    height: '85%',
     margin: '5%',
     textAlign: 'center',
     display: 'inline-block',
 };
 
-const userIconStyle = {
-    transform: "scale(2.1)"
-}
+const paperLogoStyle = {
+    maxHeight: 65,
+    paddingTop: 10
+};
 
 const messageIconStyle = {
     transform: "scale(1.0)"
-}
+};
+
+const userIconStyle = {
+    transform: "scale(2.1)"
+};
 
 class App extends Component {
+
     /* Dev Note: Will automatically fire the prop actions, or http request, once component mounts */
     componentDidMount() {
         this.props.authenticate();
+        setInterval(() => {
+            this.setState({
+                currentDateAndTime: new Date().toUTCString()
+            })
+        }, 1000)
     }
 
     constructor(props) {
         super(props);
         this.state = {
             "open": false,
-            "show": null
+            "show": null,
+            transactionCode: 'DRE01'
         };
     }
 
     handleToggle = () => this.setState({open: !this.state.open});
 
     showMainView = () => {
-        this.setState({show: 'home', open: false});
-    };
-
-    showShippingView = () => {
-        this.setState({show: 'shippingview', open: false});
-    };
-
-    showReceivingView = () => {
-        this.setState({show: 'receivingview', open: false});
-    };
-
-    showBillOfMaterials = () => {
-        this.setState({show: 'billofmaterials', open: false});
-    };
-
-    showDocumentSendView = () => {
-        this.setState({show: 'documentsendview', open: false});
+        this.setState({show: 'home', open: false, transactionCode: 'DRE01'});
     };
 
     showTrackAndTraceResultsView = () => {
         //this.setState({showProgressLogo: true}); to show blocnetsLogo before submit
         //this.setState({showProgressLogo: false}); to show blocnetsLogo after receiving response
-        this.setState({show: 'trackandtraceresultsview', open: false});
+        this.setState({show: 'trackandtraceresultsview', open: false, transactionCode: 'TT01'});
+    };
+
+    showShippingView = () => {
+        this.setState({show: 'shippingview', open: false, transactionCode: 'SHP01'});
+    };
+
+    showReceivingView = () => {
+        this.setState({show: 'receivingview', open: false, transactionCode: 'REC01'});
+    };
+
+    showBillOfMaterials = () => {
+        this.setState({show: 'billofmaterials', open: false, transactionCode: 'BOM01'});
+    };
+
+    showDocumentSendView = () => {
+        this.setState({show: 'documentsendview', open: false, transactionCode: 'DSR01'});
     };
 
     render() {
+
         let content = null;
 
-        const AppBarLogoStyle = {
-            maxWidth: 100,
-            maxHeight: 100,
-            paddingTop: 10
-        }
-
-        const paperLogoStyle = {
-            maxHeight: 65,
-            paddingTop: 10
-        }
-
         switch (this.state.show) {
+            case 'trackandtraceresultsview':
+                content = (<TrackAndTraceResultsView/>);
+                break;
             case 'shippingview':
                 content = (<ShippingView/>);
                 break;
@@ -124,9 +134,6 @@ class App extends Component {
                 break;
             case 'documentsendview':
                 content = (<DocumentSendView/>);
-                break;
-            case 'trackandtraceresultsview':
-                content = (<TrackAndTraceResultsView/>);
                 break;
             default:
                 content = (
@@ -141,25 +148,28 @@ class App extends Component {
             <div className="App">
                 <MuiThemeProvider theme={theme}>
                     {/* Main navigation bar menu for components */}
-                    <AppBar position="static"
-                            className="App-header"
+                    <AppBar position="static" className="App-header"
                             iconClassNameRight="muidocs-icon-navigation-expand-more"
                             onLeftIconButtonClick={this.handleToggle}>
                         <Grid container spacing={24}>
                             <Grid item xs={3}>
-                                <ToolbarTitle text={<img src={logo} className="App-logo" alt="logo"/>} alt="Blocnets"/>
+                                <ToolbarTitle
+                                    text={<img src={logo} className="App-logo" alt="logo"/>}
+                                    alt="Blocnets"
+                                />
                             </Grid>
                             <Grid item xs={4}>
-                                <FormControl fullWidth>
+                                <FormControl fullWidth={true}>
                                     <InputLabel htmlFor="search-with-icon-adornment">Search</InputLabel>
                                     <Input
                                         id="search-with-icon-adornment"
-                                        label="Search field"
                                         type="search"
                                         endAdornment={
                                             <InputAdornment position="end">
-                                                <SearchIcon onClick={this.showTrackAndTraceResultsView}
-                                                            style={{"cursor": "pointer"}}/>
+                                                <SearchIcon
+                                                    onClick={this.showTrackAndTraceResultsView}
+                                                    style={{"cursor": "pointer"}}
+                                                />
                                             </InputAdornment>
                                         }
                                     />
@@ -173,20 +183,20 @@ class App extends Component {
                                 </IconButton>
                             </Grid>
                             <Grid item xs={2}>
-                                <UserIcon style={userIconStyle}/>
+                                <UserIcon
+                                    style={userIconStyle}
+                                />
                             </Grid>
                         </Grid>
                     </AppBar>
                 </MuiThemeProvider>
                 {/* Side Drawer's navigation bar menu for viewing content */}
-                <Drawer
-                    docked={false}
-                    width={200}
-                    open={this.state.open}
-                    onRequestChange={(open) => this.setState({open})}>
+                <Drawer docked={false} width={250} open={this.state.open}
+                        onRequestChange={(open) => this.setState({open})}>
                     <AppBar
                         className="App-bar"
-                        title={<img src={appBarLogo} style={AppBarLogoStyle} alt="Blocnets"/>}
+                        onClick={this.handleToggle}
+                        title={<img src={logo} style={appBarLogoStyle} alt="Blocnets"/>}
                     />
                     <MenuItem id="showShippingViewId" onClick={this.showShippingView}>Shipping</MenuItem>
                     <hr/>
@@ -195,20 +205,47 @@ class App extends Component {
                     <MenuItem id="showBillOfMaterialsId" onClick={this.showBillOfMaterials}>Bill of Materials</MenuItem>
                     <hr/>
                     <MenuItem id="showDocumentSendViewId" onClick={this.showDocumentSendView}>Document
-                        Review/Entry</MenuItem>
+                        Review and Entry</MenuItem>
                 </Drawer>
-                {/* Page View with content loaded*/}
+                {/* Page View with content loaded */}
                 <Paper className="White-theme" style={paperStyle} zDepth={5}>
-
                     <Toolbar style={{"justifyContent": "center", "height": 80}}>
-                        <ToolbarTitle text={<img src={paperLogo} style={paperLogoStyle} alt="Blocnets"/>}/>
+                        <ToolbarTitle
+                            text={<img src={paperLogo} style={paperLogoStyle} alt="Blocnets"/>}
+                        />
                     </Toolbar>
                     {content}
-
                 </Paper>
+                <div style={{padding: 24}}>
+                    <Grid container spacing={24}>
+                        <MuiThemeProvider theme={theme}>
+                            <Grid container item xs={12}>
+                                <Grid container item xs>
+                                    <Typography align="right" style={{"width": "100%"}}>
+                                        {this.state.transactionCode} | System Number
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid container item xs={12}>
+                                <Grid container item xs>
+                                    <Typography align="left" style={{"width": "100%"}}>
+                                        © 2018 ALL RIGHTS RESERVED.
+                                    </Typography>
+                                </Grid>
+                                <Grid container item xs>
+                                    <Typography align="right" style={{"width": "100%"}}>
+                                        {this.state.currentDateAndTime}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </MuiThemeProvider>
+                    </Grid>
+                </div>
             </div>
         );
+
     }
+
 }
 
 /*
@@ -216,6 +253,7 @@ App.propTypes = {
   authenticate: PropTypes.func.isRequired,
 };
  */
+
 const mapStateToProps = (state) => {
     return {
         state
