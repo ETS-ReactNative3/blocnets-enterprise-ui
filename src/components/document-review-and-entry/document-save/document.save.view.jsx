a4d9b2132438e4deeaff59f23a038fa0bdff398d
John O. Morales-Marquez <jmarquez.cs@gmail.com>
2018-10-10 19:51:03 -0400

    Fixed GEO CSS
    Fixed GEO CSS

---
+++ src/components/document-review-and-entry/document-save/document.save.view.jsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import blocnetsLogo from '../../../blocknetwhite-1.png';
+import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl/FormControl';
+import FormLabel from '@material-ui/core/FormLabel/FormLabel';
+import Select from '@material-ui/core/Select/Select';
+import Input from '@material-ui/core/Input/Input';
+import MenuItem from '@material-ui/core/MenuItem/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText/FormHelperText';
+import TextField from 'material-ui/TextField';
+import Button from '@material-ui/core/Button';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import yellow from '@material-ui/core/colors/yellow';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import Snackbar from 'material-ui/Snackbar';
+import { connect } from 'react-redux';
+
+class SaveDocumentView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showProgressLogo: false,
+            file: '',
+            userName: '',
+            snackbar: {
+                autoHideDuration: 2000,
+                message: '',
+                open: false,
+                sbColor: 'black'
+            }
+        };
+    }
+
+    handleBase64File = (event) => {
+        Promise.resolve(this.setState({ fileKey: this.guid(), file: event }))
+            .then(() => {
+                console.log('State Change Log: ' + this.state.file);
+            })
+            .catch(() => {
+                console.log('State Failed to Set!');
+            })
+    }
+
+    handleUploadError = (event) => {
+        switch (event.target.error.code) {
+            case event.target.error.NOT_FOUND_ERR:
+                alert('File Not Found!');
+                break;
+            case event.target.error.NOT_READABLE_ERR:
+                alert('File is unreadable!');
+                break;
+            case event.target.error.ABORT_ERR:
+                break;
+            default:
+                alert('Error occurred while reading file!');
+        }
+        ;
+    }
+
+    handleProgressBar = (event) => {
+        if (event.lengthComputable) {
+            var percentLoaded = Math.round((event.loaded / event.total) * 100);
+            if (percentLoaded < 100) {
+                console.log(percentLoaded + '%');
+                //progress.style.width = percentLoaded + '%';
+                //progress.textContent = percentLoaded + '%';
+            }
+        }
+    }
+
+    handleFileChange = (event) => {
+        //progress.style.width = '0%';
+        //progress.textContent = '0%';
+        let files = event.target.files;
+        var file = files[0];
+        let base64Result = (string) => {
+            this.handleBase64File(string);
+        };
+        var reader = new FileReader();
+        reader.onerror = this.handleUploadError;
+        reader.onprogress = this.handleProgressBar;
+        reader.onabort = function (e) {
+            alert('File read cancelled!');
+        };
+        reader.onloadstart = function (e) {
+            // TODO: Write a mechanism that begins 'loading' - icon/progress bar
+        };
+        reader.onload = function () {
+            // Binary String
+            // var binaryString = reader.result;
+            // console.log(btoa(binaryString));
+            base64Result(reader.result);
+        };
+        reader.readAsDataURL(file);
+    }
+
+    render() {
+
+        const buttonThemeYellow = createMuiTheme({
+            palette: {
+                primary: yellow
+            },
+        });
+
+        const formComplete = this.state.recipientUserName && this.state.messageType
+            && this.state.dataType && this.state.message;
+
+        return (
+            <form>
+                <div>
+                    {this.state.showProgressLogo ?
+                        <div className="overlay"><img src={blocnetsLogo} className="App-logo-progress" alt="" />
+                        </div> : ""}
+                </div>
+                <div style={{ padding: 24 }}>
+                    <Grid container spacing={24}>
+                        <Grid container item xs={6} sm={3}>
+                            <FormLabel style={{ "textAlign": "left" }}>Recipient User Name</FormLabel>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={24}>
+                        <Grid container item xs={12} sm={6} justify="flex-end">
+                            <Grid>
+                                <input
+                                    style={{ 'display': 'none' }}
+                                    id="flat-button-file"
+                                    //multiple
+                                    type="file"
+                                    onChange={this.handleFileChange}
+                                />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </div>
+                <Snackbar
+                    open={this.state.snackbar.open}
+                    message={this.state.snackbar.message}
+                    autoHideDuration={this.state.snackbar.autoHideDuration}
+                    onRequestClose={this.handleSnackbarClose}
+                    bodyStyle={{ backgroundColor: this.state.snackbar.sbColor }}
+                />
+            </form>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        data: state,
+    };
+};
+
+// This way, we can call our action creator by doing this.props.fetchData(url);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getUserMessageDataByUserID: (url) => dispatch(getUserMessageDataByUserID(url)),
+        updateUserMessageDataByUserID: (url, body) => dispatch(updateUserMessageDataByUserID(url, body)),
+        uploadFileByUserId: (url, body) => dispatch(uploadFileByUserId(url, body))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SaveDocumentView);
--- src/components/geolocation/views/google.maps.view.js
+++ src/components/geolocation/views/google.maps.view.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { connect } from 'react-redux';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-const style = {
+const containerStyles = {
+  height: 978,
   width: '100%',
-  height: '100%'
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'center',
+  padding: 0
 }
 
-// ...
+const mapContainerStyle = {
+  left: 285,
+  right: 285,
+  bottom: 400,
+  height: 978
+};
 
 export class MapContainer extends Component {
 
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {},
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    };
+  }
 
   onMarkerClick = (props, marker, e) =>
     this.setState({
@@ -35,19 +50,15 @@ export class MapContainer extends Component {
 
   render() {
     return (
-      <Map google={this.props.google} zoom={14} style={style}>
-
-        <Marker onClick={this.onMarkerClick}
-          name={'Current location'}
-          position={{ lat: 37.778519, lng: -122.405640 }} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-        </InfoWindow>
-      </Map>
+      <Grid container spacing={24}>
+        <div style={containerStyles} >
+            <Map google={this.props.google} zoom={14} style={mapContainerStyle}>
+              <Marker onClick={this.onMarkerClick} name={'Current location'} position={{ lat: 37.778519, lng: -122.405640 }} />
+            </Map>
+        </div>
+      </Grid>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyAl04D9xFHXdrMejHnTBqh4wwYhT0FJQ-8')
-})(MapContainer)
+export default GoogleApiWrapper({ apiKey: ('') })(MapContainer)
