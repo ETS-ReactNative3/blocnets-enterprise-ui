06089da2f480437de2630349ab336477aef9b18f
John O. Morales-Marquez <jmarquez.cs@gmail.com>
2018-10-12 17:43:11 -0400

    Fixed Responsiveness for GEO mapping
    Fixed Responsiveness for GEO mapping

--- src/components/geolocation/views/google.maps.view.js
+++ src/components/geolocation/views/google.maps.view.js
@@ -2,22 +2,6 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-const containerStyles = {
-  height: 978,
-  width: '100%',
-  display: 'flex',
-  flexFlow: 'row nowrap',
-  justifyContent: 'center',
-  padding: 0
-}
-
-const mapContainerStyle = {
-  left: 285,
-  right: 285,
-  bottom: 400,
-  height: 978
-};
-
 export class MapContainer extends Component {
 
   constructor(props) {
@@ -49,10 +33,10 @@ export class MapContainer extends Component {
   render() {
     return (
       <Grid container spacing={24}>
-        <div style={containerStyles} >
-            <Map google={this.props.google} zoom={14} style={mapContainerStyle}>
-              <Marker onClick={this.onMarkerClick} name={'Current location'} position={{ lat: 37.778519, lng: -122.405640 }} />
-            </Map>
+        <div className='map-frame' >
+          <Map google={this.props.google} zoom={14} className='map-container'>
+            <Marker onClick={this.onMarkerClick} name={'Current location'} position={{ lat: 37.778519, lng: -122.405640 }} />
+          </Map>
         </div>
       </Grid>
     );
