d54499e5ff6849026ae2f920cd6dd3ef88689a85
John O. Morales-Marquez <jmarquez.cs@gmail.com>
2018-11-27 17:56:13 -0500

    RELEASE-240
    RELEASE-240

--- src/redux/actions/tree.spawn.action.js
+++ src/redux/actions/tree.spawn.action.js
@@ -33,7 +33,7 @@ export function createConstruct(materialID) {
                         .then((response) => {
                             let construct = {
                                 name: key,
-                                attributes: { ipAddress: response.data.ipAddress },
+                                attributes: { MaterialID: response.data.materialID },
                                 children: [],
                             };
 
